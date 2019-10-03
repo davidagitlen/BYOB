dcf8b40eda516b6ace95b27c9b45056e51f9e3d4
davidagitlen <davidagitlen@gmail.com>
2019-10-02 20:22:17 -0600

    Initial commit

---
+++ people-data.js
@@ -0,0 +1,609 @@
+let people = [{
+  name: 'Jocasta Nu',
+  height: '167',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Coruscant'
+},
+{
+  name: 'Ratts Tyerell',
+  height: '79',
+  mass: '15',
+  birth_year: 'unknown',
+  homeworld: 'Aleen Minor'
+},
+{
+  name: 'R4-P17',
+  height: '96',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'unknown'
+},
+{
+  name: 'Wat Tambor',
+  height: '193',
+  mass: '48',
+  birth_year: 'unknown',
+  homeworld: 'Skako'
+},
+{
+  name: 'San Hill',
+  height: '191',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Muunilinst'
+},
+{
+  name: 'Shaak Ti',
+  height: '178',
+  mass: '57',
+  birth_year: 'unknown',
+  homeworld: 'Shili'
+},
+{
+  name: 'Grievous',
+  height: '216',
+  mass: '159',
+  birth_year: 'unknown',
+  homeworld: 'Kalee'
+},
+{
+  name: 'Tarfful',
+  height: '234',
+  mass: '136',
+  birth_year: 'unknown',
+  homeworld: 'Kashyyyk'
+},
+{
+  name: 'Raymus Antilles',
+  height: '188',
+  mass: '79',
+  birth_year: 'unknown',
+  homeworld: 'Alderaan'
+},
+{
+  name: 'Sly Moore',
+  height: '178',
+  mass: '48',
+  birth_year: 'unknown',
+  homeworld: 'Umbara'
+},
+{
+  name: 'Eeth Koth',
+  height: '171',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Iridonia'
+},
+{
+  name: 'Adi Gallia',
+  height: '184',
+  mass: '50',
+  birth_year: 'unknown',
+  homeworld: 'Coruscant'
+},
+{
+  name: 'Saesee Tiin',
+  height: '188',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Iktotch'
+},
+{
+  name: 'Yarael Poof',
+  height: '264',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Quermia'
+},
+{
+  name: 'Plo Koon',
+  height: '188',
+  mass: '80',
+  birth_year: '22BBY',
+  homeworld: 'Dorin'
+},
+{
+  name: 'Mas Amedda',
+  height: '196',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Champala'
+},
+{
+  name: 'Gregar Typho',
+  height: '185',
+  mass: '85',
+  birth_year: 'unknown',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Cordé',
+  height: '157',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Cliegg Lars',
+  height: '183',
+  mass: 'unknown',
+  birth_year: '82BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'Poggle the Lesser',
+  height: '183',
+  mass: '80',
+  birth_year: 'unknown',
+  homeworld: 'Geonosis'
+},
+{
+  name: 'Anakin Skywalker',
+  height: '188',
+  mass: '84',
+  birth_year: '41.9BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'Wilhuff Tarkin',
+  height: '180',
+  mass: 'unknown',
+  birth_year: '64BBY',
+  homeworld: 'Eriadu'
+},
+{
+  name: 'Chewbacca',
+  height: '228',
+  mass: '112',
+  birth_year: '200BBY',
+  homeworld: 'Kashyyyk'
+},
+{
+  name: 'Han Solo',
+  height: '180',
+  mass: '80',
+  birth_year: '29BBY',
+  homeworld: 'Corellia'
+},
+{
+  name: 'Greedo',
+  height: '173',
+  mass: '74',
+  birth_year: '44BBY',
+  homeworld: 'Rodia'
+},
+{
+  name: 'Jabba Desilijic Tiure',
+  height: '175',
+  mass: '1,358',
+  birth_year: '600BBY',
+  homeworld: 'Nal Hutta'
+},
+{
+  name: 'Wedge Antilles',
+  height: '170',
+  mass: '77',
+  birth_year: '21BBY',
+  homeworld: 'Corellia'
+},
+{
+  name: 'Jek Tono Porkins',
+  height: '180',
+  mass: '110',
+  birth_year: 'unknown',
+  homeworld: 'Bestine IV'
+},
+{
+  name: 'Yoda',
+  height: '66',
+  mass: '17',
+  birth_year: '896BBY',
+  homeworld: 'unknown'
+},
+{
+  name: 'Palpatine',
+  height: '170',
+  mass: '75',
+  birth_year: '82BBY',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Luke Skywalker',
+  height: '172',
+  mass: '77',
+  birth_year: '19BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'C-3PO',
+  height: '167',
+  mass: '75',
+  birth_year: '112BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'R2-D2',
+  height: '96',
+  mass: '32',
+  birth_year: '33BBY',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Darth Vader',
+  height: '202',
+  mass: '136',
+  birth_year: '41.9BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'Leia Organa',
+  height: '150',
+  mass: '49',
+  birth_year: '19BBY',
+  homeworld: 'Alderaan'
+},
+{
+  name: 'Owen Lars',
+  height: '178',
+  mass: '120',
+  birth_year: '52BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'Beru Whitesun lars',
+  height: '165',
+  mass: '75',
+  birth_year: '47BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'R5-D4',
+  height: '97',
+  mass: '32',
+  birth_year: 'unknown',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'Biggs Darklighter',
+  height: '183',
+  mass: '84',
+  birth_year: '24BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'Obi-Wan Kenobi',
+  height: '182',
+  mass: '77',
+  birth_year: '57BBY',
+  homeworld: 'Stewjon'
+},
+{
+  name: 'Boba Fett',
+  height: '183',
+  mass: '78.2',
+  birth_year: '31.5BBY',
+  homeworld: 'Kamino'
+},
+{
+  name: 'IG-88',
+  height: '200',
+  mass: '140',
+  birth_year: '15BBY',
+  homeworld: 'unknown'
+},
+{
+  name: 'Bossk',
+  height: '190',
+  mass: '113',
+  birth_year: '53BBY',
+  homeworld: 'Trandosha'
+},
+{
+  name: 'Lando Calrissian',
+  height: '177',
+  mass: '79',
+  birth_year: '31BBY',
+  homeworld: 'Socorro'
+},
+{
+  name: 'Lobot',
+  height: '175',
+  mass: '79',
+  birth_year: '37BBY',
+  homeworld: 'Bespin'
+},
+{
+  name: 'Ackbar',
+  height: '180',
+  mass: '83',
+  birth_year: '41BBY',
+  homeworld: 'Mon Cala'
+},
+{
+  name: 'Mon Mothma',
+  height: '150',
+  mass: 'unknown',
+  birth_year: '48BBY',
+  homeworld: 'Chandrila'
+},
+{
+  name: 'Arvel Crynyd',
+  height: 'unknown',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'unknown'
+},
+{
+  name: 'Wicket Systri Warrick',
+  height: '88',
+  mass: '20',
+  birth_year: '8BBY',
+  homeworld: 'Endor'
+},
+{
+  name: 'Nien Nunb',
+  height: '160',
+  mass: '68',
+  birth_year: 'unknown',
+  homeworld: 'Sullust'
+},
+{
+  name: 'Shmi Skywalker',
+  height: '163',
+  mass: 'unknown',
+  birth_year: '72BBY',
+  homeworld: 'Tatooine'
+},
+{
+  name: 'Darth Maul',
+  height: '175',
+  mass: '80',
+  birth_year: '54BBY',
+  homeworld: 'Dathomir'
+},
+{
+  name: 'Bib Fortuna',
+  height: '180',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Ryloth'
+},
+{
+  name: 'Ayla Secura',
+  height: '178',
+  mass: '55',
+  birth_year: '48BBY',
+  homeworld: 'Ryloth'
+},
+{
+  name: 'Dud Bolt',
+  height: '94',
+  mass: '45',
+  birth_year: 'unknown',
+  homeworld: 'Vulpter'
+},
+{
+  name: 'Gasgano',
+  height: '122',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Troiken'
+},
+{
+  name: 'Ben Quadinaros',
+  height: '163',
+  mass: '65',
+  birth_year: 'unknown',
+  homeworld: 'Tund'
+},
+{
+  name: 'Mace Windu',
+  height: '188',
+  mass: '84',
+  birth_year: '72BBY',
+  homeworld: 'Haruun Kal'
+},
+{
+  name: 'Ki-Adi-Mundi',
+  height: '198',
+  mass: '82',
+  birth_year: '92BBY',
+  homeworld: 'Cerea'
+},
+{
+  name: 'Kit Fisto',
+  height: '196',
+  mass: '87',
+  birth_year: 'unknown',
+  homeworld: 'Glee Anselm'
+},
+{
+  name: 'Luminara Unduli',
+  height: '170',
+  mass: '56.2',
+  birth_year: '58BBY',
+  homeworld: 'Mirial'
+},
+{
+  name: 'Barriss Offee',
+  height: '166',
+  mass: '50',
+  birth_year: '40BBY',
+  homeworld: 'Mirial'
+},
+{
+  name: 'Dormé',
+  height: '165',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Dooku',
+  height: '193',
+  mass: '80',
+  birth_year: '102BBY',
+  homeworld: 'Serenno'
+},
+{
+  name: 'Bail Prestor Organa',
+  height: '191',
+  mass: 'unknown',
+  birth_year: '67BBY',
+  homeworld: 'Alderaan'
+},
+{
+  name: 'Jango Fett',
+  height: '183',
+  mass: '79',
+  birth_year: '66BBY',
+  homeworld: 'Concord Dawn'
+},
+{
+  name: 'Zam Wesell',
+  height: '168',
+  mass: '55',
+  birth_year: 'unknown',
+  homeworld: 'Zolan'
+},
+{
+  name: 'Dexter Jettster',
+  height: '198',
+  mass: '102',
+  birth_year: 'unknown',
+  homeworld: 'Ojom'
+},
+{
+  name: 'Lama Su',
+  height: '229',
+  mass: '88',
+  birth_year: 'unknown',
+  homeworld: 'Kamino'
+},
+{
+  name: 'Taun We',
+  height: '213',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Kamino'
+},
+{
+  name: 'Qui-Gon Jinn',
+  height: '193',
+  mass: '89',
+  birth_year: '92BBY',
+  homeworld: 'unknown'
+},
+{
+  name: 'Nute Gunray',
+  height: '191',
+  mass: '90',
+  birth_year: 'unknown',
+  homeworld: 'Cato Neimoidia'
+},
+{
+  name: 'Finis Valorum',
+  height: '170',
+  mass: 'unknown',
+  birth_year: '91BBY',
+  homeworld: 'Coruscant'
+},
+{
+  name: 'Jar Jar Binks',
+  height: '196',
+  mass: '66',
+  birth_year: '52BBY',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Roos Tarpals',
+  height: '224',
+  mass: '82',
+  birth_year: 'unknown',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Rugor Nass',
+  height: '206',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Ric Olié',
+  height: '183',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Watto',
+  height: '137',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'Toydaria'
+},
+{
+  name: 'Sebulba',
+  height: '112',
+  mass: '40',
+  birth_year: 'unknown',
+  homeworld: 'Malastare'
+},
+{
+  name: 'Quarsh Panaka',
+  height: '183',
+  mass: 'unknown',
+  birth_year: '62BBY',
+  homeworld: 'Naboo'
+},
+{
+  name: 'Tion Medon',
+  height: '206',
+  mass: '80',
+  birth_year: 'unknown',
+  homeworld: 'Utapau'
+},
+{
+  name: 'Finn',
+  height: 'unknown',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'unknown'
+},
+{
+  name: 'Rey',
+  height: 'unknown',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'unknown'
+},
+{
+  name: 'Poe Dameron',
+  height: 'unknown',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'unknown'
+},
+{
+  name: 'BB8',
+  height: 'unknown',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'unknown'
+},
+{
+  name: 'Captain Phasma',
+  height: 'unknown',
+  mass: 'unknown',
+  birth_year: 'unknown',
+  homeworld: 'unknown'
+},
+{
+  name: 'Padmé Amidala',
+  height: '165',
+  mass: '45',
+  birth_year: '46BBY',
+  homeworld: 'Naboo'
+}];
---
+++ planets-data.js
@@ -0,0 +1,366 @@
+let planets = [{
+  name: 'Chandrila',
+  day_length: '20',
+  year_length: '368',
+  population: '1200000000'
+},
+{
+  name: 'Sullust',
+  day_length: '20',
+  year_length: '263',
+  population: '18500000000'
+},
+{
+  name: 'Toydaria',
+  day_length: '21',
+  year_length: '184',
+  population: '11000000'
+},
+{
+  name: 'Malastare',
+  day_length: '26',
+  year_length: '201',
+  population: '2000000000'
+},
+{
+  name: 'Dathomir',
+  day_length: '24',
+  year_length: '491',
+  population: '5200'
+},
+{
+  name: 'Ryloth',
+  day_length: '30',
+  year_length: '305',
+  population: '1500000000'
+},
+{
+  name: 'Aleen Minor',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Vulpter',
+  day_length: '22',
+  year_length: '391',
+  population: '421000000'
+},
+{
+  name: 'Troiken',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Tund',
+  day_length: '48',
+  year_length: '1770',
+  population: '0'
+},
+{
+  name: 'Corellia',
+  day_length: '25',
+  year_length: '329',
+  population: '3000000000'
+},
+{
+  name: 'Rodia',
+  day_length: '29',
+  year_length: '305',
+  population: '1300000000'
+},
+{
+  name: 'Nal Hutta',
+  day_length: '87',
+  year_length: '413',
+  population: '7000000000'
+},
+{
+  name: 'Dantooine',
+  day_length: '25',
+  year_length: '378',
+  population: '1000'
+},
+{
+  name: 'Bestine IV',
+  day_length: '26',
+  year_length: '680',
+  population: '62000000'
+},
+{
+  name: 'Ord Mantell',
+  day_length: '26',
+  year_length: '334',
+  population: '4000000000'
+},
+{
+  name: 'unknown',
+  day_length: '0',
+  year_length: '0',
+  population: 'unknown'
+},
+{
+  name: 'Trandosha',
+  day_length: '25',
+  year_length: '371',
+  population: '42000000'
+},
+{
+  name: 'Socorro',
+  day_length: '20',
+  year_length: '326',
+  population: '300000000'
+},
+{
+  name: 'Mon Cala',
+  day_length: '21',
+  year_length: '398',
+  population: '27000000000'
+},
+{
+  name: 'Alderaan',
+  day_length: '24',
+  year_length: '364',
+  population: '2000000000'
+},
+{
+  name: 'Yavin IV',
+  day_length: '24',
+  year_length: '4818',
+  population: '1000'
+},
+{
+  name: 'Hoth',
+  day_length: '23',
+  year_length: '549',
+  population: 'unknown'
+},
+{
+  name: 'Dagobah',
+  day_length: '23',
+  year_length: '341',
+  population: 'unknown'
+},
+{
+  name: 'Bespin',
+  day_length: '12',
+  year_length: '5110',
+  population: '6000000'
+},
+{
+  name: 'Endor',
+  day_length: '18',
+  year_length: '402',
+  population: '30000000'
+},
+{
+  name: 'Naboo',
+  day_length: '26',
+  year_length: '312',
+  population: '4500000000'
+},
+{
+  name: 'Coruscant',
+  day_length: '24',
+  year_length: '368',
+  population: '1000000000000'
+},
+{
+  name: 'Kamino',
+  day_length: '27',
+  year_length: '463',
+  population: '1000000000'
+},
+{
+  name: 'Geonosis',
+  day_length: '30',
+  year_length: '256',
+  population: '100000000000'
+},
+{
+  name: 'Haruun Kal',
+  day_length: '25',
+  year_length: '383',
+  population: '705300'
+},
+{
+  name: 'Cerea',
+  day_length: '27',
+  year_length: '386',
+  population: '450000000'
+},
+{
+  name: 'Glee Anselm',
+  day_length: '33',
+  year_length: '206',
+  population: '500000000'
+},
+{
+  name: 'Iridonia',
+  day_length: '29',
+  year_length: '413',
+  population: 'unknown'
+},
+{
+  name: 'Tholoth',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Iktotch',
+  day_length: '22',
+  year_length: '481',
+  population: 'unknown'
+},
+{
+  name: 'Quermia',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Dorin',
+  day_length: '22',
+  year_length: '409',
+  population: 'unknown'
+},
+{
+  name: 'Champala',
+  day_length: '27',
+  year_length: '318',
+  population: '3500000000'
+},
+{
+  name: 'Mirial',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Serenno',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Concord Dawn',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Zolan',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Ojom',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: '500000000'
+},
+{
+  name: 'Skako',
+  day_length: '27',
+  year_length: '384',
+  population: '500000000000'
+},
+{
+  name: 'Muunilinst',
+  day_length: '28',
+  year_length: '412',
+  population: '5000000000'
+},
+{
+  name: 'Shili',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Kalee',
+  day_length: '23',
+  year_length: '378',
+  population: '4000000000'
+},
+{
+  name: 'Umbara',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Tatooine',
+  day_length: '23',
+  year_length: '304',
+  population: '200000'
+},
+{
+  name: 'Jakku',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Utapau',
+  day_length: '27',
+  year_length: '351',
+  population: '95000000'
+},
+{
+  name: 'Mustafar',
+  day_length: '36',
+  year_length: '412',
+  population: '20000'
+},
+{
+  name: 'Kashyyyk',
+  day_length: '26',
+  year_length: '381',
+  population: '45000000'
+},
+{
+  name: 'Polis Massa',
+  day_length: '24',
+  year_length: '590',
+  population: '1000000'
+},
+{
+  name: 'Mygeeto',
+  day_length: '12',
+  year_length: '167',
+  population: '19000000'
+},
+{
+  name: 'Felucia',
+  day_length: '34',
+  year_length: '231',
+  population: '8500000'
+},
+{
+  name: 'Cato Neimoidia',
+  day_length: '25',
+  year_length: '278',
+  population: '10000000'
+},
+{
+  name: 'Saleucami',
+  day_length: '26',
+  year_length: '392',
+  population: '1400000000'
+},
+{
+  name: 'Stewjon',
+  day_length: 'unknown',
+  year_length: 'unknown',
+  population: 'unknown'
+},
+{
+  name: 'Eriadu',
+  day_length: '24',
+  year_length: '360',
+  population: '22000000000'
+}];
